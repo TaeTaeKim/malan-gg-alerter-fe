5ca48ade93847d07031ecde0031b78441df165e9
김태윤 <mygod456@kakao.com>
2025-06-10 03:44:18 +0900

    <fix> : 등록된 아이템 뷰 option 그리드 디자인 대응

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="item-card">
-<!--    아이템 이미지, 이름, 설정가격, 알림여부를 결정하는 화면-->
+    <!--    아이템 이미지, 이름, 설정가격, 알림여부를 결정하는 화면-->
     <div class="item-card-info">
       <img :src="item.imageUrl" alt="" class="item-img"/>
       <div class="item-card-name-price">
         <div class="item-card-name">
-          <a :href="`https://mapleland.gg/item/${ item.itemId }`" class="malan-gg-link" target="_blank" rel="noopener noreferrer">
-            <span>{{item.koreanName}}</span>
+          <a :href="`https://mapleland.gg/item/${ item.itemId }`" class="malan-gg-link" target="_blank"
+             rel="noopener noreferrer">
+            <span>{{ item.koreanName }}</span>
             <img src="https://mapleland.gg/logo.png" alt="메랜지지로고" class="malan-gg-logo">
           </a>
         </div>
@@ -16,7 +17,7 @@
         </div>
       </div>
       <div class="item-card-alarm">
-        <div >
+        <div>
           <img src="@/assets/question-mark.png" class="item-card-alarm-info" alt="">
         </div>
         <div class="item-card-alarm-btn">
@@ -49,7 +50,12 @@
       <!-- Other options grid -->
       <div class="options-grid">
         <div v-for="opt in otherOptions" :key="opt.key" class="option-cell">
-          <p>{{ opt.label }}: {{ item.option[opt.key] }}</p>
+          <div class="option-label">
+            {{ opt.label }}
+          </div>
+          <div class="option-value">
+            {{ item.option[opt.key] }}
+          </div>
         </div>
       </div>
     </div>
@@ -60,12 +66,17 @@
   </div>
 </template>
 <script setup>
-import { itemOptions } from '@/constants/itemOptions'
+import {itemOptions} from '@/constants/itemOptions'
+
 const props = defineProps({
   item: Object
 })
-const otherOptions = itemOptions.filter(opt => opt.key !== 'price')
-
+const otherOptions = itemOptions
+    .filter(opt => opt.key !== 'price')
+    .map(opt => ({
+      ...opt,
+      label: opt.label.length > 2 ? opt.label.substring(0, 2) : opt.label
+    }))
 </script>
 
 <style scoped>
@@ -75,7 +86,7 @@ const otherOptions = itemOptions.filter(opt => opt.key !== 'price')
   padding: 10px;
   background: #343741;
   --tw-bg-opacity: 1;
-  box-shadow: 0 2px 8px rgba(0,0,0,0.03);
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.03);
   transition: box-shadow 0.2s;
   height: 245px;
   width: 100%;
@@ -83,51 +94,63 @@ const otherOptions = itemOptions.filter(opt => opt.key !== 'price')
   box-sizing: border-box;
   line-height: 0.5;
 }
-.item-card-info{
+
+.item-card-info {
   display: flex;
   justify-content: space-between;
   height: 50px;
   padding-bottom: 20px;
   border-bottom: 1px solid #484B56;
 }
-.item-img{
+
+.item-img {
   height: 42px;
   width: 42px;
+  margin-right: 1vw;
 }
-.malan-gg-logo{
+
+.malan-gg-logo {
   height: 20px;
   padding-left: 4px;
 }
-.malan-gg-link{
+
+.malan-gg-link {
   color: white;
 }
-.item-card-name-price{
+
+.item-card-name-price {
   display: grid;
   align-items: center;
   width: 70%;
   height: 100%;
 }
-.item-card-name{
+
+.item-card-name {
   font-weight: bolder;
   font-size: 20px;
+  padding-bottom: 10px;
   height: 20px;
 }
+
 .item-card-price {
   display: flex;
   justify-content: start;
   align-items: center;
 }
+
 .options-container {
   display: flex;
   flex-direction: column;
   gap: 10px;
   margin-top: 10px;
 }
-.item-card-alarm{
+
+.item-card-alarm {
   display: flex;
   align-items: start;
 }
-.item-card-alarm-info{
+
+.item-card-alarm-info {
   padding-top: 5px;
   padding-right: 2px;
   height: 20px;
@@ -171,7 +194,8 @@ const otherOptions = itemOptions.filter(opt => opt.key !== 'price')
 .toggle-container.active .toggle-button {
   transform: translateX(32px);
 }
-.icon-sound-off,.icon-sound-on{
+
+.icon-sound-off, .icon-sound-on {
   position: absolute;
   top: 48%;
   transform: translateY(-50%);
@@ -180,32 +204,54 @@ const otherOptions = itemOptions.filter(opt => opt.key !== 'price')
   opacity: 0.6;
   z-index: 1;
 }
+
 .icon-sound-on {
   left: 6px;
 }
+
 .icon-sound-off {
   right: 6px;
 }
+
 .toggle-container.active .icon-sound-on {
   opacity: 1;
 }
+
 .toggle-container:not(.active) .icon-sound-off {
   opacity: 1;
 }
+
+.options-container {
+  height: 50%;
+}
+
 .options-grid {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+  align-items: center;
+  justify-content: center;
+  height: 100%;
   gap: 3px;
 }
 
 .option-cell {
+  display: flex;
   font-size: 0.9em;
   height: 2vh;
 }
-.item-img{
-  margin-right: 1vw;
+
+.option-label {
+  color: #8C8FA3;
+  width: 28px;
+  margin-right: 10px;
 }
-.item-btn{
+
+.option-value {
+  color: white;
+  font-weight: bold;
+}
+
+.item-btn {
   padding: 0.1em 0.4em;
   background: none;
   font-size: 1.3em;
