9e9a94b36d0246a105fa9b72658b9c5d146eed59
김태윤 <mygod456@kakao.com>
2025-06-23 11:04:46 +0900

    <hotfix> : 리프리레시가 안될경우 다른 요청을 보내지 못하도록하고 login 으로 push

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,56 +1,59 @@
 import axios from 'axios'
-import { useAuthStore } from '@/store/auth'
-import { useNotificationStore } from '@/store/noti'
+import {useAuthStore} from '@/store/auth'
+import {useNotificationStore} from '@/store/noti'
 import router from '@/router'
 
 axios.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config
 
-    // refresh token 으로 access token 갱신
-    if (isAccessTokenExpired(error) && !originalRequest._retry) {
-      originalRequest._retry = true
+        // refresh token 으로 access token 갱신
+        if (isAccessTokenExpired(error) && !originalRequest._retry) {
+            originalRequest._retry = true
+            const auth = useAuthStore()
 
-      const auth = useAuthStore()
-      try {
-        await auth.renewToken() // refresh 와 브라우저 localStorage 갱신
-        originalRequest.headers['Authorization'] = `Bearer ${auth.accessToken}`
-        return axios(originalRequest)
-      } catch (e) {
-        auth.logout()
-        return Promise.reject(e)
-      }
-    }
+            try {
+                await auth.renewToken() // refresh 와 브라우저 localStorage 갱신
+                if (auth.accessToken) {
+                    originalRequest.headers['Authorization'] = `Bearer ${auth.accessToken}`;
+                    return axios(originalRequest)
+                }
+                return Promise.reject(error)
+            } catch (e) {
+                auth.logout()
+                router.push('/login')
+                return Promise.reject(e)
+            }
+        }
 
-    // refresh 만료 혹은 찾지 못했을 떄
-    if(isInvalidRefreshToken(error)){
-        const auth = useAuthStore()
-        await auth.logout()
-        router.push('/login')
+        // refresh 만료 혹은 찾지 못했을 떄
+        if (isInvalidRefreshToken(error)) {
+            const auth = useAuthStore()
+            await auth.logout()
+            router.push('/login')
+            return Promise.reject(error)
+        }
+
+        // 500 에러 처리
+        if (error.response?.status === 500) {
+            const notificationStore = useNotificationStore()
+            notificationStore.showNotification('서버 에러가 발생했습니다.')
+        }
+
         return Promise.reject(error)
     }
-
-    // 500 에러 처리
-    if (error.response?.status === 500) {
-      const notificationStore = useNotificationStore()
-      notificationStore.showNotification('서버 에러가 발생했습니다.')
-    }
-
-    return Promise.reject(error)
-  }
-
 )
 
-function isAccessTokenExpired(error){
+function isAccessTokenExpired(error) {
     return (
         error.response &&
         error.response.status === 401 &&
-        error.response.data.code === "AUTH_002" // 액세스 토큰 만료나 로그아웃된 토큰일때
+        error.response.data.code === "AUTH_002" // 액세스 토큰 만료시
     )
 }
 
-function isInvalidRefreshToken(error){
+function isInvalidRefreshToken(error) {
     console.log(error.response)
     return (
         error.response &&
