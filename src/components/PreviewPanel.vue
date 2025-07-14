e644f1ba482299891e528c3bff043f15012ef892
김태윤 <mygod456@kakao.com>
2025-07-15 06:25:41 +0900

    <doc> PreviewPanel code convention 정의

--- src/components/PreviewPanel.vue
+++ src/components/PreviewPanel.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="preview-panel" v-if="store.currentItem">
     <div class="preview-item-name">
-      <span>{{store.currentItem.name}}</span>
-      <a :href="`https://mapleland.gg/item/${store.currentItem.id}`" target="_blank" rel="noopener noreferrer" class="preview-item-link">
+      <span>{{ store.currentItem.name }}</span>
+      <a :href="`https://mapleland.gg/item/${store.currentItem.id}`" target="_blank" rel="noopener noreferrer"
+        class="preview-item-link">
         <span>메랜지지 바로가기</span>
       </a>
-      <img src="/link.png" alt="link icon" class="preview-link-icon"/>
+      <img src="/link.png" alt="link icon" class="preview-link-icon" />
     </div>
     <div class="preview-item-info">
       <div class="preview-left">
         <div class="image-container">
-          <img :src="imageUrl" alt="preview" class="preview-img"/>
+          <img :src="imageUrl" alt="preview" class="preview-img" />
         </div>
       </div>
       <div class="preview-right">
-        <OptionSelector @submit="addItem" @back="clearCurrentItem"/>
+        <OptionSelector @submit="addItem" @back="clearCurrentItem" />
       </div>
     </div>
   </div>
@@ -27,8 +28,8 @@
 </template>
 
 <script setup>
-import {computed} from 'vue'
-import {useMainStore} from '@/store'
+import { computed } from 'vue'
+import { useMainStore } from '@/store'
 import OptionSelector from './OptionSelector.vue'
 
 const store = useMainStore()
@@ -52,51 +53,59 @@ function clearCurrentItem() {
   border: 1px solid #484B56;
   border-radius: 8px;
   background-color: #404552;
-  height: 310px; /* Fixed height instead of max-height */
+  height: 310px;
+  /* Fixed height instead of max-height */
   overflow-y: auto;
 }
-.preview-panel-msg{
+
+.preview-panel-msg {
   margin-top: 20px;
   padding: 20px 20px 26px 30px;
   border-radius: 8px;
   background: #1D1E23;
-  height: 310px; /* Fixed height instead of max-height */
+  height: 310px;
+  /* Fixed height instead of max-height */
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
-.preview-panel-msg>img{
+
+.preview-panel-msg>img {
   height: 70px;
 }
 
 /* 등록 뷰 상단 이름과 메랜지지 링크*/
-.preview-item-name{
+.preview-item-name {
   display: flex;
   justify-content: start;
   align-items: center;
 }
-.preview-item-name > span{
+
+.preview-item-name>span {
   font-weight: bolder;
   font-size: 20px;
 }
-.preview-item-link{
+
+.preview-item-link {
   margin: 0 10px 0 10px;
   font-size: 16px;
   color: #B0B5C3;
 }
 
-.preview-link-icon{
+.preview-link-icon {
   width: 16px;
   height: 16px;
 }
-.preview-item-info{
+
+.preview-item-info {
   display: flex;
   justify-content: start;
   height: 270px;
   gap: 20px;
 }
+
 /* 등록 뷰 이미지*/
 .preview-left {
   display: flex;
